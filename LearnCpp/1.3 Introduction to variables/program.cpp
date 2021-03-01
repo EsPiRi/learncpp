29ee3cd4724eb15f4dfa0cfa60ac39293da1eb9e
EsPiRi <sezginsami@gmail.com>
2021-03-01 18:22:52 +0300

    Learn some cpp coding

---
+++ LearnCpp/1.3 Introduction to variables/program.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+  // define a variable named width in single line
+  double width;
+
+  // assigning a value to a variable
+  width = 5;
+
+  // define multiple variables in multiple lines
+  int a;
+  int b;
+
+  // define multiple variables in single line (with same type!)
+  int c, d;
+
+  // direct initialization
+  int var2(2);
+
+  // list initialization
+  int var3{ 3 };
+
+  //
+}
